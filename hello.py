f5d30bd04e318c4391584fe0d05218168102d54a
Kai Zer <2605587@sit.singaporetech.edu.sg>
2026-09-04 17:30:42 +0800

    5th Activity

--- hello.py
+++ hello.py
@@ -32,3 +32,16 @@ print("====================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+username = input("Enter Username:")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print("\n Instagram Profile")
+print("=====================")
+print("Username: ", username)
+print("Age:", age)
+print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you?")
