7ae934d884bd40d6f44869e218be6500cb1f708a
Kai Zer <2605587@sit.singaporetech.edu.sg>
2026-09-04 16:53:39 +0800

    Second commit

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ print("===========================================")
 print("Welcome here")
 print("My First Post")
 print("===========================================")
+
+username = "cool creator"
+bio = "Fun blogger"
+followers = str(100)
+
+
+print("Username:" + username)
+print("Bio:" + bio)
+print("Followers:" + followers)
