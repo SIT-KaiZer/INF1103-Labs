52bbbf004c0a0b9eb3b1d5380f715fb4c19cadaa
Kai Zer <2605587@sit.singaporetech.edu.sg>
2026-09-04 17:18:12 +0800

    second commit

--- hello.py
+++ hello.py
@@ -8,6 +8,19 @@ bio = "Fun blogger"
 followers = str(100)
 
 
-print("Username:" + username)
-print("Bio:" + bio)
-print("Followers:" + followers)
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
+
+
