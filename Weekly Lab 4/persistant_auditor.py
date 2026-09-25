596560699d451d30bf3f68e94e0165f7f2809628
Kai Zer <2605587@sit.singaporetech.edu.sg>
2026-09-25 17:29:37 +0800

    Added load_orders function

--- Weekly Lab 4/persistant_auditor.py
+++ Weekly Lab 4/persistant_auditor.py
@@ -1,16 +1,20 @@
 def get_valid_input():
     print("get valid input called")
 
-def load_inventory():
-    print("load inventory called")
-    totalInventoryArray = open("inventory.txt", "r")
+def load_orders():
+    print("load orders called")
+    with open(".\Weekly Lab 4\orders.txt", "r") as file:
+        orders = file.read()
+        print(f"Current orders:\n{orders}")
     
-
-def save_inventory():
-    print("save inventory called")
+def save_orders():
+    print("save orders called")
 
 def generate_report():
     print("generate report called")
 
 while True:
-    inventory = load_inventory()
+    load_orders()
+    
+    print("test")
+    break
