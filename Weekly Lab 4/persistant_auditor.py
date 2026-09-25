a17f8a96c0ef17a739d8bcb01cdacbf7fd4f3f6d
Kai Zer <2605587@sit.singaporetech.edu.sg>
2026-09-25 17:12:23 +0800

    Added persistent_auditor.py and inventory.txt

---
+++ Weekly Lab 4/persistant_auditor.py
@@ -0,0 +1,16 @@
+def get_valid_input():
+    print("get valid input called")
+
+def load_inventory():
+    print("load inventory called")
+    totalInventoryArray = open("inventory.txt", "r")
+    
+
+def save_inventory():
+    print("save inventory called")
+
+def generate_report():
+    print("generate report called")
+
+while True:
+    inventory = load_inventory()
