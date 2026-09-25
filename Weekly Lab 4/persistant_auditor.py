a0bc4cc9803a8c5a7b3d985ca9b9ca527a88f4fd
Kai Zer <2605587@sit.singaporetech.edu.sg>
2026-09-25 17:44:23 +0800

    Added get_valid_input function

--- Weekly Lab 4/persistant_auditor.py
+++ Weekly Lab 4/persistant_auditor.py
@@ -1,9 +1,39 @@
 def get_valid_input():
     print("get valid input called")
+    while True:
+        newOrder = input("Enter Product Name (or type 'quit' to exit): ")
+
+        if newOrder.lower() == "quit":
+            return "quit"
+        
+        elif newOrder is None or newOrder.strip() == "":
+            print("Invalid input. Please enter a valid product name.")
+            continue
+
+        elif newOrder.isdigit():
+            print("Invalid input. Product name cannot be a number. Please enter a valid product name.")
+            continue
+
+        elif not newOrder.strip() == "".isalpha():
+            print("Invalid input. Product name must contain only letters. Please enter a valid product name.")
+            continue
+
+        else:
+            while True:
+                newOrderQuantity = input("Enter the quantity for the product: ")
+
+                if not newOrderQuantity.isdigit:
+                    print("Invalid input. Quantity must be a number. Please enter a valid quantity.")
+                    continue
+                else:
+                    return newOrder, int(newOrderQuantity)
+
+        
+
 
 def load_orders():
     print("load orders called")
-    with open(".\Weekly Lab 4\orders.txt", "r") as file:
+    with open(".\Weekly Lab 4\inventory.txt", "r") as file:
         orders = file.read()
         print(f"Current orders:\n{orders}")
     
@@ -15,6 +45,7 @@ def generate_report():
 
 while True:
     load_orders()
+    get_valid_input()
     
     print("test")
     break
